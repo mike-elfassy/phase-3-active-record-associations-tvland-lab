ba1a6076ebd5be0838e23c8621e8636f3743ef8b
Michael Elfassy <mike.elfassy@gmail.com>
2023-06-30 20:52:57 -0400

    commit tree 3bf893aae968d37dd7f05d47e520e9e1b481afe0

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        p "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        roles = []
+        self.characters.each do |character|
+            # binding.pry
+            roles << "#{character.name} - #{character.show.name}"
+        end
+        roles
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    actors_full_names = []
+    self.actors.each do |actor|
+        actors_full_names << actor.full_name
+    end
+    actors_full_names
+  end
 end
